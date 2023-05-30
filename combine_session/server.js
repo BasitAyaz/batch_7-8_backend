ace392d7deb4a987fc445914c610086a7ce331bb
BasitAyaz <basitahmed1997@gmail.com>
2023-05-30 21:56:14 +0500

    30-5-2023 (Authentication)

--- combine_session/server.js
+++ combine_session/server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const StudentRouter = require("./routes/studentRouter");
+const UserRoute = require("./routes/userroute");
 require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 
 app.use("/api/student", StudentRouter);
+app.use("/api/user", UserRoute);
 
 mongoose
   .connect(process.env.MONGO_URI)
